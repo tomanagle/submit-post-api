d3d4435fb60af560828b3d883401da05a37694a8
tomanagle <tomanagle@gmail.com>
2022-01-29 10:45:47 +1000

    fix: base

--- src/controller/post.controller.ts
+++ src/controller/post.controller.ts
@@ -87,7 +87,8 @@ export async function getPostsHandler(
                 ...post.media,
                 frame: post?.media?.frame || getFrame(),
                 // @ts-ignore
-                base: post.media.base || getBasePath(new Date(post.createdAt)),
+                base:
+                  post?.media?.base || getBasePath(new Date(post.createdAt)),
               },
               "500",
               "500",
